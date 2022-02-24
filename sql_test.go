f0af929da241640c5b62d9b2c38f4a5831374974
insectdie <ompusungguandry@gmail.com>
2022-02-24 16:32:20 +0700

    SQL safe injection & auto increment

--- sql_test.go
+++ sql_test.go
@@ -112,3 +112,48 @@ func TestSqlInjection(t *testing.T) {
 		fmt.Println("Gagal Login  ")
 	}
 }
+
+func TestSqlInjectionSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	username := "admin"
+	password := "admin"
+
+	ctx := context.Background()
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err = rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses Login  ", username)
+	} else {
+
+		fmt.Println("Gagal Login  ")
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	username := "andry"
+	password := "andry"
+
+	ctx := context.Background()
+	script := "INSERT INTO user(username, password) VALUES(?, ?)"
+	_, err := db.ExecContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Sukses insert new user")
+}
